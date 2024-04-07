022f34b69a3102f917c82f88e476128d52b9e8df
nectordev <p.vlad-rus@mail.ru>
2024-04-07 19:56:05 +0300

    рефакторинг

---
+++ Right on target/Right on target/Model/Game.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+protocol GameProtocol {
+    var score: Int { get }
+    var currentSecretValue: Int { get }
+    var isGameEnded: Bool { get }
+    
+    func restartGame()
+    func startNewRound()
+    func calculateScore(with value: Int)
+}
+
+class Game: GameProtocol {
+    var score: Int = 0
+    var currentSecretValue: Int = 0
+    
+    private var minSecretValue: Int
+    private var maxSecretValue: Int
+    private var lastRound: Int
+    private var currentRound: Int = 1
+    
+    var isGameEnded: Bool {
+        if currentRound >= lastRound {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init? (startValue: Int, endValue: Int, rounds: Int) {
+        guard startValue <= endValue else {
+            return nil
+        }
+        
+        minSecretValue = startValue
+        maxSecretValue = endValue
+        lastRound = rounds
+        currentSecretValue = self.getNewSecretValue()
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentSecretValue = self.getNewSecretValue()
+        currentRound += 1
+    }
+    
+    private func getNewSecretValue() -> Int {
+        (minSecretValue...maxSecretValue).randomElement()!
+    }
+    
+    func calculateScore(with value: Int) {
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+        } else {
+            score += 50
+        }
+    }
+}
--- Right on target/Right on target/ViewController.swift
+++ Right on target/Right on target/ViewController.swift
@@ -1,87 +1,35 @@
-//
-//  ViewController.swift
-//  Right on target
-//
-//  Created by Владислав Пономаренко on 04.04.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
-    lazy var secondViewController: SecondViewController = getSecondViewController()
-    
-    var number: Int = 0
-    var round: Int = 1
-    var points: Int = 0
-    
-    private func getSecondViewController() -> SecondViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(identifier: "SecondViewController")
-        return viewController as! SecondViewController
-    }
-    
-    override func loadView() {
-        super.loadView()
-        print("loadView")
-    }
+    var game: Game!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ViewDidLoad")
-        self.number = Int.random(in: 1...50)
-        self.label.text = String(number)
+        game = Game(startValue: 1, endValue: 5, rounds: 5)
+        self.label.text = String(game.currentSecretValue)
         
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDissapear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewWDidDissapear")
-    }
-    
     
     @IBAction func checkNumber() {
-
-        let numSlider = Int(self.slider.value.rounded())
-            
-        if numSlider > self.number {
-            self.points += 50 - numSlider + self.number
-        } else if numSlider < self.number {
-            self.points += 50 - self.number + numSlider
+        game.calculateScore(with: Int(slider.value))
+        
+        if game.isGameEnded {
+            showAlertWith(score: Int(slider.value))
+            game.restartGame()
         } else {
-            self.points += 50
+            game.startNewRound()
         }
         
-        if self.round == 5 {
-            let alert = UIAlertController(title: "Игра окончена", message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            self.round = 1
-            self.points = 0
-        } else {
-            self.round += 1
-        }
-            
-        self.number = Int.random(in: 0...50)
-        self.label.text = String(self.number)
+        self.label.text = String(game.currentSecretValue)
     }
     
-    
-    
-    
-
-
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(title: "Игра окончена", message: "Вы заработали \(score) очков", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
